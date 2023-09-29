8cb258f1e345de6cbba94e970e75947620b16783
Mat-Kon <mat_kon@mail.ru>
2023-09-29 16:14:21 +0300

    refactor: refactor class names

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,9 +1,17 @@
+const resultClasses = Object.freeze({
+  RESULT: 'results',
+  WRAPPER: 'results__wrapper',
+  ITEM: 'result__item',
+  NAME: 'item__name',
+  VALUE: 'item__value',
+});
+
 const Result: React.FC = () => {
   return (
-    <div className="results">
-      <div className="results__wrapper"></div>
+    <div className={resultClasses.RESULT}>
+      <div className={resultClasses.WRAPPER}></div>
     </div>
   );
 };
 
-export { Result };
+export { Result, resultClasses };
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,51 +1,82 @@
 import { Api } from '../api/api';
 import { IFilm, IPeople, IPlanet, ISpecies, IStarShips, IVehicles } from '../types/types';
+import { resultClasses } from './Results';
+
+const searchClasses = Object.freeze({
+  SEARCH: 'search',
+  HEADING: 'heading',
+  FORM: 'search__form',
+  INPUT: 'search__input',
+  BTN: 'search__btn',
+});
 
 const Search: React.FC = () => {
-  const search: React.MouseEventHandler<HTMLInputElement> = async (e) => {
-    e.preventDefault();
-    const inputElem = document.querySelector('.search__input') as HTMLInputElement;
-    const resultWrap = document.querySelector('.results__wrapper') as HTMLInputElement;
+  window.onload = () => {
+    getFromLocal();
+  };
+
+  const viewAllValue = async (): Promise<void> => {
+    const resultWrap = document.querySelector(`.${resultClasses.WRAPPER}`) as HTMLInputElement;
     resultWrap.innerHTML = '';
-    const inputValue = inputElem.value;
-    localStorage.setItem('searchValue', inputValue);
-    if (inputValue === '') {
-      const allInfo = await new Api().getAll();
-      const results: (IPeople | IPlanet | IFilm | ISpecies | IVehicles | IStarShips)[] = (
-        [] as (IPeople | IPlanet | IFilm | ISpecies | IVehicles | IStarShips)[]
-      ).concat(...allInfo);
-      results.forEach((item: IPeople | IPlanet | IFilm | ISpecies | IVehicles | IStarShips) => {
-        const resultItem = document.createElement('div');
-        resultItem.classList.add('result__item');
-        const keys = Object.keys(item);
-        const values: string | string[] | number = Object.values(item);
-        keys.forEach((key, index) => {
-          const firsValue = document.createElement('h2');
-          firsValue.classList.add('item__name');
-          const secondValue = document.createElement('p');
-          secondValue.classList.add('item__value');
-          firsValue.textContent = key;
-          let value: string;
-          value = values[index];
-          if (Array.isArray(value)) {
-            value = value.join('<br>');
-          }
-          value = value ? value : 'not';
-          secondValue.innerHTML = `${value}`;
-          resultItem.append(firsValue, secondValue);
-        });
-        resultWrap.append(resultItem);
+    const allInfo = await new Api().getAll();
+    const results: (IPeople | IPlanet | IFilm | ISpecies | IVehicles | IStarShips)[] = (
+      [] as (IPeople | IPlanet | IFilm | ISpecies | IVehicles | IStarShips)[]
+    ).concat(...allInfo);
+    results.forEach((item: IPeople | IPlanet | IFilm | ISpecies | IVehicles | IStarShips) => {
+      const resultItem = document.createElement('div');
+      resultItem.classList.add(resultClasses.ITEM);
+      const keys = Object.keys(item);
+      const values: string | string[] | number = Object.values(item);
+      keys.forEach((key, index) => {
+        const firsValue = document.createElement('h2');
+        firsValue.classList.add(resultClasses.NAME);
+        const secondValue = document.createElement('p');
+        secondValue.classList.add(resultClasses.VALUE);
+        firsValue.textContent = key;
+        let value: string;
+        value = values[index];
+        if (Array.isArray(value)) {
+          value = value.join('<br>');
+        }
+        value = value ? value : 'not';
+        secondValue.innerHTML = `${value}`;
+        resultItem.append(firsValue, secondValue);
       });
+      resultWrap.append(resultItem);
+    });
+  };
+
+  const searchValue = async (value: string): Promise<void> => {
+    const resultWrap = document.querySelector(`.${resultClasses.WRAPPER}`) as HTMLInputElement;
+    resultWrap.innerHTML = '';
+    console.log(value);
+  };
+
+  const search: React.MouseEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    const inputElem = document.querySelector(`.${searchClasses.INPUT}`) as HTMLInputElement;
+    const value: string = inputElem.value;
+    localStorage.setItem('searchValue', value);
+
+    if (value === '') {
+      viewAllValue();
+    } else {
+      searchValue(value);
     }
-    inputElem.value = '';
+  };
+
+  const getFromLocal = (): void => {
+    const input = document.querySelector(`.${searchClasses.INPUT}`) as HTMLInputElement;
+    const value = localStorage.getItem('searchValue') ?? '';
+    input.value = value;
   };
 
   return (
-    <div className="search">
-      <h1 className="heading">Star Wars Searching</h1>
-      <form className="search__form" action="search">
-        <input className="search__input" type="text" placeholder="enter" />
-        <input className="search__btn" type="submit" value="Search" onClick={search} />
+    <div className={searchClasses.SEARCH}>
+      <h1 className={searchClasses.HEADING}>Star Wars Searching</h1>
+      <form className={searchClasses.FORM} action="search">
+        <input className={searchClasses.INPUT} type="text" />
+        <input className={searchClasses.BTN} type="submit" value="Search" onClick={search} />
       </form>
     </div>
   );
