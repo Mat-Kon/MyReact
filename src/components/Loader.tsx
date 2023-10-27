7daad0014d69d67001f1a1a1093bdaaea8b0e576
Mat-Kon <mat_kon@mail.ru>
2023-10-27 11:00:12 +0300

    init: change Loader to finction

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,14 +1,11 @@
-import { Component, ReactNode } from 'react';
+import React from 'react';
 
 type Props = {
   isLoading: boolean;
 };
 
-class Loader extends Component<Props> {
-  render(): ReactNode {
-    const { isLoading } = this.props;
-    return isLoading ? <h2 className="loader">Loading</h2> : null;
-  }
-}
+const Loader: React.FC<Props> = ({ isLoading }) => {
+  return isLoading ? <h2 className="loader">Loading</h2> : null;
+};
 
 export default Loader;
