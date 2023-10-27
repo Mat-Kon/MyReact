65b8c6a99934313cc2c6fd7c98d3a57ff2f49f2c
Mat-Kon <mat_kon@mail.ru>
2023-10-27 11:13:12 +0300

    refactr: change ErrorPage and ItemBlock

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,13 +1,11 @@
-import { Component, ReactNode } from 'react';
+import React from 'react';
 
-class ErrorPage extends Component {
-  render(): ReactNode {
-    return (
-      <div className="wrapper">
-        <h1 className="error-message-2">This mistake gave victory to the Empire!</h1>
-      </div>
-    );
-  }
-}
+const ErrorPage: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <h1 className="error-message-2">This mistake gave victory to the Empire!</h1>
+    </div>
+  );
+};
 
 export default ErrorPage;
--- src/components/ItemBlock.tsx
+++ src/components/ItemBlock.tsx
@@ -1,36 +1,33 @@
-import { Component, ReactNode } from 'react';
+import React from 'react';
 import { Item } from '../types/types';
 
 type ItemProps = {
   item: Item;
 };
 
-class ItemBlock extends Component<ItemProps> {
-  render(): ReactNode {
-    const { item } = this.props;
-    return (
-      <div className="result__item-wrapper">
-        <div className="result__item">
-          {Object.entries(item!).map((value, index) => {
-            if (index < 2) {
-              return (
-                <div className="item" key={index}>
-                  <h2 className="item__name">{value[0]}</h2>
-                  <p className="item__value">
-                    {Array.isArray(value[1])
-                      ? (value[1] as string[]).map((value) => value + '\n')
-                      : String(value[1])}
-                  </p>
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
-        </div>
+const ItemBlock: React.FC<ItemProps> = ({ item }) => {
+  return (
+    <div className="result__item-wrapper">
+      <div className="result__item">
+        {Object.entries(item!).map((value, index) => {
+          if (index < 2) {
+            return (
+              <div className="item" key={index}>
+                <h2 className="item__name">{value[0]}</h2>
+                <p className="item__value">
+                  {Array.isArray(value[1])
+                    ? (value[1] as string[]).map((value) => value + '\n')
+                    : String(value[1])}
+                </p>
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ItemBlock;
