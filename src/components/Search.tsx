2b5a3aa82d8abb3a0914f151381a2ed548e09c10
Mat-Kon <mat_kon@mail.ru>
2023-10-29 19:35:53 +0300

    feat: install react-router-dom

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -14,6 +14,28 @@ const Search: React.FC = () => {
   const [maxPage, setMaxPage] = useState(1);
 
   useEffect(() => {
+    const getAllItems = async (): Promise<void> => {
+      setIsLoading(true);
+      const data: Result = await new Api().getItems(page);
+      const curMaxPage = Math.ceil(data.count / 10);
+      setMaxPage(curMaxPage);
+      setItems(data.items);
+      setIsLoading(false);
+    };
+
+    const searchItem = async (value: string) => {
+      setIsLoading(true);
+      const data: Result = await new Api().getSearchItems(value, page);
+      const curMaxPage = Math.ceil(data.count / 10);
+      if (data.items.length > 0) {
+        setItems(data.items);
+        setMaxPage(curMaxPage);
+        setIsLoading(false);
+      } else {
+        setItems(null);
+        setIsLoading(false);
+      }
+    };
     if (value) {
       searchItem(value);
     } else {
@@ -26,29 +48,6 @@ const Search: React.FC = () => {
     setPage(1);
     setIsLoading(true);
     setValue(localStorage.getItem('searchValue') ?? '');
-  };
-
-  const getAllItems = async (): Promise<void> => {
-    setIsLoading(true);
-    const data: Result = await new Api().getItems(page);
-    const curMaxPage = Math.ceil(data.count / 10);
-    setMaxPage(curMaxPage);
-    setItems(data.items);
-    setIsLoading(false);
-  };
-
-  const searchItem = async (value: string) => {
-    setIsLoading(true);
-    const data: Result = await new Api().getSearchItems(value, page);
-    const curMaxPage = Math.ceil(data.count / 10);
-    if (data.items.length > 0) {
-      setItems(data.items);
-      setMaxPage(curMaxPage);
-      setIsLoading(false);
-    } else {
-      setItems(null);
-      setIsLoading(false);
-    }
   };
 
   return (
