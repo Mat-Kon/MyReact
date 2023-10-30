6a4c115d1e8bd39a07b0e801230cd2bcca8078ab
Mat-Kon <mat_kon@mail.ru>
2023-10-30 14:33:47 +0300

    feat: install tupes for router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App';
 import { checkServerAvailability } from './api/check-server';
 import ErrorBoundary from './components/ErrorBoundary';
 import './sass/main.scss';
+import NotFoundPage from './components/NotFoundPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />,
+  },
+]);
 
 checkServerAvailability();
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <ErrorBoundary>
-    <App />
+    <RouterProvider router={router} />
   </ErrorBoundary>
 );
