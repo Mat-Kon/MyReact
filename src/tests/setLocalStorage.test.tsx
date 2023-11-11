2d70b92502cea67f8ac3c8354b672c2d839af8a4
Mat-Kon <mat_kon@mail.ru>
2023-11-11 18:25:53 +0300

    feat: add test to save value in local storage after click on Search btn

---
+++ src/tests/setLocalStorage.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../components/App';
+
+test('it show that clicking the Search button saves the entered value to the local storage', async () => {
+  const user = userEvent.setup();
+  render(<App />);
+
+  const loader = await screen.findByTestId('loader');
+  expect(loader).not.toBeNull();
+
+  await waitFor(
+    async () => {
+      const results = await screen.findByTestId('results__wrapper');
+      expect(results).toBeInTheDocument();
+    },
+    { timeout: 8000 }
+  );
+
+  const inputElement = screen.getByRole('textbox');
+  const searchButton = screen.getByRole('button', { name: /search/i });
+  await user.click(inputElement);
+  await user.keyboard('luke');
+  await user.click(searchButton);
+  expect(localStorage.getItem('searchValue')).toBe('luke');
+});
